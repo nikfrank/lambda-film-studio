60dbaefb166b9f8c84f8b270164eb8c371ab2f4b
nik frank <nikbfrank@gmail.com>
2019-08-21 13:24:40 -0700

    updates for prod

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import { futch } from './futch';
 import stub from './stub';
 
 
-const url = 'testing url';
-
 const groupStills = allFiles => {
   const filenames = allFiles.map(file => file.Key).filter(filename=> filename.match(/-out\d+\.png/));
 
@@ -42,33 +40,39 @@ function App() {
   const [ isLoggedIn, setIsLoggedIn ] = useState(false);
   const [ password, setPassword ] = useState('');
   const [ films, setFilms ] = useState([]);
-  
+
+  const loadStills = ()=> fetch('/test/film-list').then(response => response.json())
+                                                  .then(stills=> setFilms( groupStills(stills.Contents) ));
+
   const login = ()=> fetch('/test/login', {
     method: 'POST', headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ password })
 
   }).then(response => response.status > 400 ? console.error(401) : setIsLoggedIn( true ))
-
-
-  useEffect(()=> setFilms( groupStills(stub.Contents) ), []);
-
+    .then(loadStills);
+  
   const upload = ()=>{
-    const body = new FormData();
-    body.append(
-      'file',
-      document.querySelector('input[type=file]').files[0]
-    );
 
-    futch(url, {
-      method: 'PUT',
-      body,
-      headers: { "Content-Type": "multipart/form-data" },
-      
-    }, e=> {
-      console.log('uploaded', e.loaded / e.total, '%');
-      
-    }).then(res=> console.log('upload done'))
-      .catch(err=> console.log('upload failed with', err));
+    const file = document.querySelector('input[type=file]').files[0];
+    
+    fetch('/test/s3-upload', { method: 'POST', body: JSON.stringify({ filename: file.name }), credentials: 'include' })
+      .then(response=> response.text())
+      .then(url=> {
+        
+        const body = new FormData();
+        body.append('file', file);
+
+        futch(url, {
+          method: 'PUT',
+          body,
+          headers: { "Content-Type": "multipart/form-data" },
+          
+        }, e=> {
+          console.log('uploaded', e.loaded / e.total, '%');
+          
+        }).then(res=> (console.log('upload done'), loadStills()))
+          .catch(err=> console.log('upload failed with', err));
+      });
   };
 
   const onDragEnd = result => {
