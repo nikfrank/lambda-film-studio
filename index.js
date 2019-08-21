64f8db71d168e380575bac074dbfb350435c3f2c
nik frank <nikbfrank@gmail.com>
2019-08-21 13:51:42 -0700

    loading favicon, mime types

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 exports.handler = (event, context)=>{
   
-  context.done(null, {
-    "statusCode": 200,
-    "headers":{
-      "Content-Type": "text/html",
-    },
-    "body": require('fs').readFileSync('./build'+event.path).toString()
-  });
+  event.path.match(/favicon.ico/) ? (
+    context.done(null, {
+      "statusCode": 200,
+      "headers":{
+        "Content-Type": 'image/x-icon',
+      },
+      "body": require('fs').readFileSync('./build'+event.path).toString('base64'),
+      "isBase64Encoded": true,
+    })
+  ) : (
+    context.done(null, {
+      "statusCode": 200,
+      "headers":{
+        "Content-Type": ("text/" + (event.path.match(/\.css$/) ? 'css' : event.path.match(/\.js$/) ? 'javascript' : 'html')),
+      },
+      "body": require('fs').readFileSync('./build'+event.path).toString()
+    })
+  );
 }
